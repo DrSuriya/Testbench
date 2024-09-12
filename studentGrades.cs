52bb505eb7d31345286602a7ece09a4a1e0effe1
DrSuriya <155653384+DrSuriya@users.noreply.github.com>
2024-09-11 20:23:58 -0400

    Update studentGrades.cs

--- studentGrades.cs
+++ studentGrades.cs
@@ -3,9 +3,21 @@ int currentAssignments = 5;
 int numberStudents = 4;
 
 List<int> sophia = new List<int> { 93, 87, 98, 95, 100 };
-List<int> nicolas = new List<int> { 93, 87, 98, 95, 100 };
-List<int> zahirah = new List<int> { 93, 87, 98, 95, 100 };
-List<int> jeong = new List<int> { 93, 87, 98, 95, 100 };
+List<int> nicolas = new List<int> { 80, 83, 82, 88, 85 };
+List<int> zahirah = new List<int> { 84, 96, 73, 85, 79 };
+List<int> jeong = new List<int> { 90, 92, 98, 100, 97 };
+
+static string getLetterScore(double average) =>
+        average switch
+        {
+            >= 96 => "A+",
+            > 93 => "A",
+            > 90 => "A-",
+            > 87 => "B+",
+            > 83 => "B",
+            _ => "F",
+        };
+
 
 List<List<int>> studentsList = new List<List<int>> { sophia, nicolas, zahirah, jeong };
 List<string> studentNames = new List<string>{"Sophia", "Nicolas", "Zahirah", "Jeong"};
@@ -23,5 +35,5 @@ for (int i = 0; i < numberStudents; i++)
     }
     
     float average = (float)sum / currentAssignments; 
-    Console.WriteLine($"{name}\t\t{average:F2}");
+    Console.WriteLine($"{name}\t\t{average} {getLetterScore(average)}");
 }
