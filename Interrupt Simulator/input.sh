9ce9d8763fa2a828c53bde2e5f653e5f9c775d91
DrSuriya <155653384+DrSuriya@users.noreply.github.com>
2025-03-02 17:17:34 -0500

    Delete Interrupt Simulator directory

--- Interrupt Simulator/input.sh
+++
@@ -1,5 +0,0 @@
-#chmod +x test1.sh
-#./test1.sh
-gcc interrupts.cpp -o interrupts
-./interrupts trace1.txt execution1.txt vector_table.txt
-#./interrupts trace1.txt execution1.txt additionalFiles/vector_table.txt
