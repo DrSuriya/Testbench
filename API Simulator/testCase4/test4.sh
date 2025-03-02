bda945235413abceb20f62a4c98e8dfd4ba4c6ac
DrSuriya <155653384+DrSuriya@users.noreply.github.com>
2025-03-02 17:17:27 -0500

    Delete API Simulator directory

--- API Simulator/testCase4/test4.sh
+++
@@ -1,4 +0,0 @@
-#chmod +x test4.sh
-#./test4.sh
-g++ interrupts.cpp -o simulator
-./simulator trace.txt external_files.txt vector_table1.txt output.txt
--- API Simulator/testCase5/test5.sh
+++
@@ -1,4 +0,0 @@
-#chmod +x test5.sh
-#./test5.sh
-g++ interrupts.cpp -o simulator
-./simulator trace.txt external_files.txt vector_table1.txt output.txt
